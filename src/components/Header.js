7de33fd25e71f6c4bccc65216370bcbf00de8354
codeclassifiers <codeclassifiers@gmail.com>
2020-09-26 07:17:44 +0530

    template added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="w-100 text-light">
+                Jobs Portal
+            </div>
+        </nav>
+    );
+}
+
+export default Header;
